d635e9e2d22e3b21ace5445ae294e3dc5876b0a3
praveen.ampati@kpipartners.com <praveen.ampati@kpipartners.com>
2023-06-02 10:53:38 +0000

    raw&stg

---
+++ models/stg/stage_orders.sql
@@ -0,0 +1,18 @@
+select o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice-o.ordercustprice as orderprofit,
+c.customerid,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+from 
+    {{ ref('raw_orders') }} as o
+    left outer join {{ ref('raw_customer') }} as c
+        on o.customerid=c.customerid
+    left outer join {{ ref('raw_product') }} as p
+        on o.productid=p.productid
